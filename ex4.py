959efe2cbd4fab5e0f40f14b38464be287fe5133
lukasxlabrie <126988962+Misosucky@users.noreply.github.com>
2023-07-23 15:32:13 -0400

    added notes to lines 1 - 9

--- ex4.py
+++ ex4.py
@@ -1,8 +1,9 @@
-cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
-cars_not_driven = cars - drivers
-cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
-average_passengers_per_car = passengers / cars_driven
+cars = 100 #defines var cars
+space_in_a_car = 4.0 #defines var space in a car
+drivers = 30 #defines var drivers
+passengers = 90 #defines var passengers
+cars_not_driven = cars - drivers #prompts subtraction
+cars_driven = drivers #redefines var drivers
+carpool_capacity = cars_driven * space_in_a_car #prompts multiplication
+average_passengers_per_car = passengers / cars_driven #prompts division
+#to create a varaible you simply write an = statemenet DO NOT use ()
